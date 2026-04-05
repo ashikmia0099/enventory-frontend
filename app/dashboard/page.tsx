74880e0313e91f15ca983f8a1e6aac1b2c227489
Ashik Ahammed <129544444+ashikmia0099@users.noreply.github.com>
2026-04-05 09:59:50 +0600

    login register add

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function SidebarPage() {
     <ProtectedRoute role="ADMIN">
       <div className="p-6 space-y-6">
         <h1 className="text-3xl font-bold">Dashboard</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className=" gap-6">
           <AllCard />
         </div>
         <div className="mt-6">
